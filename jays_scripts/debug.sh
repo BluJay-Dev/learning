8e943ec276e4c1d0e8bea0795879babf516dbb13
Oleg Bocharov <oleg.bocharov@mirrorweb.com>
2021-07-28 21:50:51 +0100

    Comments on debug.sh

--- jays_scripts/debug.sh
+++ jays_scripts/debug.sh
@@ -1,9 +1,28 @@
-#!/bin/bash 
+#!/usr/bin/env bash
 
-DEBUG=true 
+# It's for compatibility, not all OS and destribution have `/bin/bash` path
+
+DEBUG="${DEBUG:-false}"
+# Always use double qutes round shell variables
+#
+# ${VAR:-default} reads VARS from env first, if empty or not defined uses "default" value
+
 if $DEBUG 
 then 
     echo "Debug mode on."
 else 
     echo "Debug mode off"
 fi 
+
+# Referances
+# - GNU Bash default variables
+#   https://www.gnu.org/software/bash/manual/html_node/Bash-Variables.html
+# - Shebang:Portability
+#   https://en.wikipedia.org/wiki/Shebang_(Unix)#Portability
+# - Quoting
+#   https://wiki.bash-hackers.org/syntax/quoting
+# - Parameter Expansion
+#   https://wiki.bash-hackers.org/syntax/pe
+# - For insperation
+#   https://github.com/netdata/netdata
+#   https://git.savannah.gnu.org/cgit/guix.git/tree/etc/guix-install.sh
